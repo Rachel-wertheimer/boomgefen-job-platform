c6f67b3cfe8d0cfb233392c5bd63c973df55598d
rachel <rw4803213@gmail.com>
2025-10-29 00:46:53 +0200

    Fix: User name display, implement forgot password, add ad content editing for managers

--- support-api/routes/users.js
+++ support-api/routes/users.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 router.post('/createUser', CreateUser);
 router.post("/login", FindUserByEmail);
+// router.post('/forgot-password', forgotPassword);
 router.get('/getDetails/:user_id', getDetails);
+router.put('/subscription_start/:user_id', updateSubscriptionController);
 router.put('/subscription_start/:user_id', updateSubscriptionController);
 router.delete('/deleteUser/:user_id', deleteUser);
 
