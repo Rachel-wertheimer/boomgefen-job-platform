c5976103581a57fa391667e0542158dce8cfe81b
rachel <rw4803213@gmail.com>
2025-10-30 01:12:04 +0200

    update

--- support-dashboard/src/app/store.ts
+++ support-dashboard/src/app/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adsReducer from "./slice/adsSlice";
 import userReducer from "./slice/userSlice";
-import mailReducer from "./slice/mailSlice";
+// import mailReducer from "./slice/mailSlice";
 export const store = configureStore({
   reducer: {
     ads: adsReducer,
     user: userReducer,
-    mail: mailReducer
+    // mail: mailReducer
   },
 });
 
