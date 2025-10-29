f7870c1b3f9b2de370508006cb76eb8a60d2fa40
rachel <rw4803213@gmail.com>
2025-10-30 01:41:24 +0200

    update start

--- support-api/server.js
+++ support-api/server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const cors = require("cors");
-const port = process.env.PORT || 3001;
-
 require("dotenv").config();
 
 const adsRouter = require('./routes/ads');
 const usersRouter = require('./routes/users');
 const userProfilesRouter = require('./routes/user_profiles');
-const emailRoute = require('./routes/email')
+const emailRoute = require('./routes/email');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const server = require('http').createServer(app);
 
 app.use('/api/v1/ads', adsRouter);
-app.use('/api/v1/users',usersRouter );
-app.use('/api/v1/user_profiles',userProfilesRouter );
-app.use('/api/v1/email',emailRoute );
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/user_profiles', userProfilesRouter);
+app.use('/api/v1/email', emailRoute);
 
+const port = process.env.PORT || 3001;
 
-server.listen(port);
-
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
