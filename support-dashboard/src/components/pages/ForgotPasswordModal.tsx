8b608b070ca9b1731775764730135399b55677e7
rachel <rw4803213@gmail.com>
2025-10-30 05:06:36 +0200

    add checkbox

--- support-dashboard/src/components/pages/ForgotPasswordModal.tsx
+++ support-dashboard/src/components/pages/ForgotPasswordModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { FaSpinner } from "react-icons/fa";
 import { appColors } from "../../utils/colors";
