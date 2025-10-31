f9191096dc92c84d263190108ea3169278a61de2
rachel <rw4803213@gmail.com>
2025-10-31 02:26:46 +0200

    forget password

--- support-api/functionUserProfile.js
+++ support-api/functionUserProfile.js
@@ -9,18 +9,9 @@ exports.insertUserDetails = async (details, userId) => {
   return result;
 };
 
-// exports.getUserByEmail = async (email) => {
-//   const [rows] = await pool.query(
-//     `SELECT email
-//      FROM users
-//      WHERE LOWER(email) = ?`,
-//     [email.toLowerCase()]
-//   );
-//   return rows[0];
-// };
 exports.getUserByEmail = async (email) => {
   const [rows] = await pool.query(
-    `SELECT id, email, temporary_password
+    `SELECT email
      FROM users
      WHERE LOWER(email) = ?`,
     [email.toLowerCase()]
@@ -28,15 +19,25 @@ exports.getUserByEmail = async (email) => {
   return rows[0];
 };
 
-
-exports.updateTemporaryPassword = async (code,userId) => {
+exports.updateTemporaryPassword = async (code, userId) => {
+  // קודם מעדכנים את הקוד
   await pool.query(
     `UPDATE user_profiles SET temporary_password = ? WHERE user_id = ?`,
     [code, userId]
   );
+
+  // עכשיו שולפים את הערך המעודכן
+  const [rows] = await pool.query(
+    `SELECT temporary_password FROM user_profiles WHERE user_id = ?`,
+    [userId]
+  );
+
+  // מחזירים את הערך של temporary_password
+  return rows[0]?.temporary_password;
 };
 
-exports.updatePassword = async (hashedPassword ,userId) => {
+
+exports.updatePassword = async (hashedPassword, userId) => {
   await pool.query(
     `UPDATE user_profiles 
      SET password = ?, temporary_password = NULL 
