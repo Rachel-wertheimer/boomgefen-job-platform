493e9cd47a8c49316b090ec24c2d1d082cc8d3fd
rachel <rw4803213@gmail.com>
2025-11-23 18:14:54 +0200

    Style

--- support-dashboard/src/components/pages/WorkWithUs.tsx
+++ support-dashboard/src/components/pages/WorkWithUs.tsx
@@ -170,10 +170,13 @@ export default function WorkWithUs() {
             await createUserProfile(profilePayload, newUserId);
             console.log('User profile created successfully.');
           
-            window.location.href = "https://pay.sumit.co.il/g9687k/gg7p5h/hykun7/payment/";
+            // window.location.href = "https://pay.sumit.co.il/g9687k/gg7p5h/hykun7/payment/";
         } catch (err) {
             alert('אירעה שגיאה בשליחת הטופס. נסי שוב.');
             setLoading(false);
+        }
+        finally{
+            alert('תודה על הרשמתך! ניצור איתך קשר בהקדם.');
         }
     };
 
